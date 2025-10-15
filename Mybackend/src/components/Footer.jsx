5cfa568c4c4f59fd6d4fa6d395d3ad950a0e94de
Simon-898 <sim.arevalo@duocuc.cl>
2025-10-15 00:54:31 -0300

    se logra levantar la pagina de iniciov1

---
+++ Mybackend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto">
+      <img src="/images/footer.jpg" alt="Footer" className="img-fluid w-100" />
+    </footer>
+  );
+}
+
+export default Footer;
--- Mybackend/src/main.jsx
+++ Mybackend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+// Estilos globales
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles/global.css';
+
+// JS de Bootstrap (para dropdowns, carruseles, etc.)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
