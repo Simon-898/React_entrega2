86426ad92a903bd2d6015299757f9e99124c62b6
Simon-898 <sim.arevalo@duocuc.cl>
2025-10-15 02:51:09 -0300

    se agrego, contacto y nosotros

--- Mybackend/src/App.jsx
+++ Mybackend/src/App.jsx
@@ -1,8 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Blogs from './pages/Blogs';
+// src/App.jsx
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
+import Home from "./pages/Home.jsx";
+import Blogs from "./pages/Blogs.jsx";
+import Nosotros from "./pages/Nosotros.jsx";
+import Contacto from "./pages/Contacto.jsx";
 
 export default function App() {
   return (
@@ -12,6 +17,11 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blogs" element={<Blogs />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/contacto" element={<Contacto />} />
+
+        
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
       <Footer />
