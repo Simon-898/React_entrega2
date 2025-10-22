e08918706a2bb521c753a131ecfb2eade2ac5050
Simon-898 <sim.arevalo@duocuc.cl>
2025-10-22 17:35:58 -0300

    Delete Mybackend directory

--- Mybackend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <div className="text-center">
-        <p className="mb-0">© 2025 Chavito. Todos los derechos reservados.</p>
-      </div>
-    </footer>
-  );
-}
